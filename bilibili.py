fb9beb9944b583ab669d207bbee1a0cbf6ff35c8
unknown <1765155167@eqq.com>
2019-10-19 17:51:54 +0800

    v1.1

--- bilibili.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://www.bilibili.com"
-res = requests.get(url)
-res.encoding = 'utf-8'
-soup = BeautifulSoup(res, 'html.parser')
-print(soup)
