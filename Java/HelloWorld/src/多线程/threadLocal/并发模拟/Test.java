fd15cd093f29f7b413c5d50810679a52dd749437
Hqf <1765155167@qq.com>
2020-03-10 23:11:21 +0800

    add ThreadLocal

---
+++ Java/HelloWorld/src/多线程/threadLocal/并发模拟/Test.java
@@ -0,0 +1,25 @@
+package 多线程.threadLocal.并发模拟;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newCachedThreadPool();
+        StatController stat = new StatController();
+        for (int i = 0; i < 100 ; i++) {
+            service.submit(()->{
+                for (int j = 0; j < 100; j++) {
+                    stat.add();
+                }
+            });
+        }
+        service.shutdown();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(stat.start());
+    }
+}
---
+++ Java/HelloWorld/src/多线程/threadLocal/并发模拟/Var.java
@@ -0,0 +1,13 @@
+package 多线程.threadLocal.并发模拟;
+
+public class Var<T> {
+    T v;
+
+    public T get() {
+        return v;
+    }
+
+    public void set(T v) {
+        this.v = v;
+    }
+}
